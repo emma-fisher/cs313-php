cce54bb8b5f8d72048914fcbc3b06761f6a26ebf
Emma Fisher <emmarfisher99@gmail.com>
2018-05-03 09:27:34 -0600

    Added intro

---
+++ web/intro.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>Introduction Page </h1>
+        <h2>Thank you for visiting my webpage. </h2>
+        <h3>Here you will learn a little more about ice cream </h3>
+        <?php
+      
+        ?>
+    </body>
+</html>
