8fa57374a93d30b813adecd6b5556fbd1666ff84
Emma Fisher <emmarfisher99@gmail.com>
2018-05-04 21:50:16 -0600

    Add image

--- web/assignments.php
+++ web/assignments.php
@@ -4,7 +4,7 @@
         <title>Assignments Page</title>
         <link rel="stylesheet" href="main.css">
     </head>
-    <body class="assignBody">
+    <body>
         <h3>Coming Soon...</h3>
     </body>
 </html>
