3d12957d69154e1c88b345ae82f379593e48aba5
Emma Fisher <emmarfisher99@gmail.com>
2018-05-05 18:23:35 -0600

    Changes

--- web/intro.php
+++ web/intro.php
@@ -14,7 +14,7 @@
         <h2>Emma Fisher</h2>
         <h3>Welcome to my webpage! I'm Emma, a Computer Science 
         student currently attending BYU-Idaho.</h3>
-        
+        <div class="daysToBday">
         <?php
             $date = strtotime("September 21, 2018 6:00 AM");
             $remaining = $date - time();
@@ -25,7 +25,9 @@
             echo "Time until my next birthday: $days_remaining days $hours_remaining hours $minutes_remaining minutes $seconds_remaining seconds";
         ?>
         </div>
-        <img src="https://scontent-sea1-1.xx.fbcdn.net/v/t1.0-9/19225525_1386650188069841_3093724628678236927_n.jpg?_nc_cat=0&oh=99d91432eca6b5f6988a58a7e5458ef7&oe=5B4F9288")>
+        </div>
         <button onclick="location.href='assignments.php'" type="button">Click Here</button>
+        <img src="https://scontent-sea1-1.xx.fbcdn.net/v/t1.0-9/19225525_1386650188069841_3093724628678236927_n.jpg?_nc_cat=0&oh=99d91432eca6b5f6988a58a7e5458ef7&oe=5B4F9288")>
+       
     </body>
 </html>
