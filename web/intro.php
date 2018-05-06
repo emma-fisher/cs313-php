6f435c55a65f996d268e23b002d2731af41efd6c
Emma Fisher <emmarfisher99@gmail.com>
2018-05-05 19:32:07 -0600

    Changes

--- web/intro.php
+++ web/intro.php
@@ -14,8 +14,9 @@
         <h2>Emma Fisher</h2>
         <h3>Welcome to my webpage! I'm Emma, a Computer Science 
         student currently attending BYU-Idaho.</h3>
+        <div>
         <img id="bearPic" src="https://scontent-sea1-1.xx.fbcdn.net/v/t1.0-9/19225525_1386650188069841_3093724628678236927_n.jpg?_nc_cat=0&oh=99d91432eca6b5f6988a58a7e5458ef7&oe=5B4F9288")>
-
+        </div>
         <div class="daysToBday">
         <?php
             $date = strtotime("September 21, 2018 6:00 AM");
