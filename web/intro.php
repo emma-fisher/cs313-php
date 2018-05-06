2ec3db4bd8b867fe68ef495a8d8709b527c5c277
Emma Fisher <emmarfisher99@gmail.com>
2018-05-05 19:17:15 -0600

    Changes

--- web/intro.php
+++ web/intro.php
@@ -14,6 +14,7 @@
         <h2>Emma Fisher</h2>
         <h3>Welcome to my webpage! I'm Emma, a Computer Science 
         student currently attending BYU-Idaho.</h3>
+        <a href="assignments.php">Click Here to see my Assignments Page!</a>
         <div class="daysToBday">
         <?php
             $date = strtotime("September 21, 2018 6:00 AM");
@@ -27,7 +28,7 @@
         </div>
         
         </div>
-        <a href="assignments.php">Click Here to see my Assignments Page!</a>
+        
         <img class="img" src="https://scontent-sea1-1.xx.fbcdn.net/v/t1.0-9/19225525_1386650188069841_3093724628678236927_n.jpg?_nc_cat=0&oh=99d91432eca6b5f6988a58a7e5458ef7&oe=5B4F9288")>
        
     </body>
