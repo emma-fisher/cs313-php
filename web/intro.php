f781c085eb9c744e83e0f03ff8b146f4a89e1a38
Emma Fisher <emmarfisher99@gmail.com>
2018-05-05 20:44:08 -0600

    Changes

--- web/intro.php
+++ web/intro.php
@@ -18,7 +18,7 @@
         </div>
         <img id="bearPic" src="https://scontent-sea1-1.xx.fbcdn.net/v/t1.0-9/19225525_1386650188069841_3093724628678236927_n.jpg?_nc_cat=0&oh=99d91432eca6b5f6988a58a7e5458ef7&oe=5B4F9288")>
 
-        <div class="daysToBday">
+        <div id="box1">
         <?php
             $date = strtotime("September 21, 2018 6:00 AM");
             $remaining = $date - time();
@@ -26,9 +26,9 @@
             $hours_remaining = floor(($remaining % 86400) / 3600);
             $minutes_remaining = floor(($remaining % 3600) / 60);
             $seconds_remaining = ($remaining % 60);
-            echo "<div>Time until my next birthday: $days_remaining days $hours_remaining hours $minutes_remaining minutes $seconds_remaining seconds;</div>"        ?>
+            echo "<div style ='font:COurier New,sans-serif'>Time until my next birthday: $days_remaining days $hours_remaining hours $minutes_remaining minutes $seconds_remaining seconds;</div>"        ?>
         </div>
-        <div>
+        <div id="box2">
         <a href="assignments.php">Click Here to see my Assignments Page!</a>
         </div>
     </body>
