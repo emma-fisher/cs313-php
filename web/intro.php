1c931a68e2182d26f9d85750717c5af6ce4ca29c
Emma Fisher <emmarfisher99@gmail.com>
2018-05-05 21:16:25 -0600

    Changes

--- web/intro.php
+++ web/intro.php
@@ -13,7 +13,9 @@
         <div>
         <h2>Emma Fisher</h2>
         <h3>Welcome to my webpage! I'm Emma, a Computer Science 
-        student currently attending BYU-Idaho.</h3>
+        student currently attending BYU-Idaho. I am from California and this is
+        my third semester. I really love to play tennis and spend time with friends and
+        family. Also, I think bears are cool, hence the picture below.</h3>
         
         </div>
         <img id="bearPic" alt="Here I am at Bear World." src="https://scontent-sea1-1.xx.fbcdn.net/v/t1.0-9/19225525_1386650188069841_3093724628678236927_n.jpg?_nc_cat=0&oh=99d91432eca6b5f6988a58a7e5458ef7&oe=5B4F9288")>
