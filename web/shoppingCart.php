c5c63304869eee950ffd62f8282dc13646de5a6d
Emma Fisher <emmarfisher99@gmail.com>
2018-05-08 18:03:12 -0600

    Adding shopping cart

---
+++ web/shoppingCart.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="shoppingCart.css">
+        <title>Shopping Cart</title>
+    </head>
+    <body>
+        <div id="header">Welcome to The Ice Cream Shop!</div>
+        <div id="container">
+            <div> Mint Chip</div>
+            <div>Vanilla</div>
+            <div>Chocolate</div>
+            <div>Banana</div>
+            <div>Bubblegum</div>
+            <div>Oreo</div>
+            <div>Cookie Dough</div>
+            <div>Cherry Chip</div>
+            <div>Neopolitan</div>
+            <div>Mint Chip</div>
+            <div></div>
+            <div>Rainbow Sherbet</div>
+        </div>
+    </body>
+</html>
