b80130de8cb3b4b0ac201a294cc030f9fde8a27e
Emma Fisher <emmarfisher99@gmail.com>
2018-05-08 18:52:04 -0600

    Adding shopping cart

--- web/shoppingCart.php
+++ web/shoppingCart.php
@@ -1,7 +1,23 @@
 
 
  <?php
-            $flavor = htmlspecialchars($_POST["name"]);
+    $flavor = htmlspecialchars($_POST["name"]);
 
-            echo "Your flavor is " . $flavor;
-        ?>
+    echo "Your flavor is " . $flavor;
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Submission Results</title>
+</head>
+
+<body>
+	<h1>Submission Results</h1>
+
+	<p>Your name is: <?=$flavor ?></p>
+
+</body>
+
+
+</html>
