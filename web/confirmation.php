0c52b9a0eca15cb1d98d2e49ddf0444200895cd9
Emma Fisher <emmarfisher99@gmail.com>
2018-05-12 22:09:08 -0600

    shopping cart

--- web/confirmation.php
+++ web/confirmation.php
@@ -9,17 +9,17 @@
     <?php 
     
         $firstname = htmlspecialchars($_POST["firstname"]);
-        echo 'First Name: ' . $firstname;
+        echo 'First Name: ' . $firstname . '<br>';
         $lastname = htmlspecialchars($_POST["lastname"]);
-        echo 'Last Name: ' . $lastname;
+        echo 'Last Name: ' . $lastname . '<br>';
         $street = htmlspecialchars($_POST["street"]);
-        echo 'Street: ' . $street;
+        echo 'Street: ' . $street . '<br>';
         $city = htmlspecialchars($_POST["city"]);
-        echo 'City: ' . $city;
+        echo 'City: ' . $city . '<br>';
         $state = htmlspecialchars($_POST["state"]);
-        echo 'State: ' . $state;
+        echo 'State: ' . $state . '<br>';
         $zip = htmlspecialchars($_POST["zip"]);
-        echo 'Zip Code: ' . $zip;
+        echo 'Zip Code: ' . $zip . '<br>';
     ?>
 
     <p>Thank you for shopping!</p>
