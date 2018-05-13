c42d2a4c6a8d2b40aa9d5e7eb50a4d4a7370e63f
Emma Fisher <emmarfisher99@gmail.com>
2018-05-12 19:38:48 -0600

    shopping cart

--- web/shopping-cart.php
+++ web/shopping-cart.php
@@ -6,11 +6,23 @@
 
 <body>
 	<h1>Submission Results</h1>
-	The flavor you added is: <?php 
+	The flavor you added is: 
+	<?php 
+	$items = array();
 	foreach($_POST[flavors] as $flavor) {
-            echo $flavor . ", ";
+			echo $flavor . ", ";
+			array_push($items, $flavor);
 		}
-		?>
+
+	$Rows = 4; //Dynamic number for Rowss
+	$Cols = 3; // Dynamic number for Colsumns
+	echo '<table border="1">';
+	for($i=1;$i<=$Rows;$i++){ echo '<tr>';
+	  for($j=1;$j<=$Cols;$j++){ echo '<td>' . mt_rand($i, $i*100) . mt_rand($j, $j*100) . '</td>'; }
+		  echo '</tr>';
+	  }
+	echo '</table>';
+	?>
 
 </body>
 
