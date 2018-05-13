bc09fa035ca3e12a90dad21ac4a5e9cb60a982b4
Emma Fisher <emmarfisher99@gmail.com>
2018-05-12 20:36:09 -0600

    shopping cart

--- web/shopping-cart.php
+++ web/shopping-cart.php
@@ -15,11 +15,12 @@
 		}
 
 	$Rows = sizeof($items); //Dynamic number for Rowss
-	$Cols = 2; // Dynamic number for Columns
+	$Cols = 3; // Dynamic number for Columns
 	echo '<table border="1">';
 	echo '<tr>';
 	echo '<th>Item Name</th>';
 	echo '<th>Quantity</th>';
+	echo '<th>Delete</th>';
 	echo '</tr>';
 	
 	for($i=0;$i<$Rows;$i++)
@@ -29,11 +30,39 @@
 		for($j=1;$j<$Cols;$j++)
 		{ 
 			 echo '<td>' . 1 . '</td>';
+			 echo '<td>' . '<a href="?delete=<?php echo($i); ?>">Delete from cart</a>' . '</td>';
 		}
 		echo '</tr>';
 	}
 	echo '</table>';
+
+	if ( isset($_GET["delete"]) )
+ {
+   $i = $_GET["delete"];
+   $qty = $_SESSION["qty"][$i];
+   $qty--;
+   $_SESSION["qty"][$i] = $qty;
+
+ //remove item if quantity is zero
+ if ($qty == 0) {
+   $_SESSION["amounts"][$i] = 0;
+   unset($_SESSION["cart"][$i]);
+ }
+ else
+ {
+   $_SESSION["amounts"][$i] = $amounts[$i] * $qty;
+ }
+ }
 	?>
+
+	<a href="shopping-cart.html">Keep shopping</a>
+	<a href="">Check out</a>
+
+	<script>
+		function deleteItem() {
+
+		}
+	</script>
 
 </body>
 
