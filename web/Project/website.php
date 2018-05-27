a953be6854cc9ebeb38383ddccd41d330be6c24e
Emma Fisher <emmarfisher99@gmail.com>
2018-05-26 19:01:11 -0600

    database project

--- web/Project/website.php
+++ web/Project/website.php
@@ -22,12 +22,22 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <script src="js/uikit.min.js"></script>
+        <script src="js/uikit-icons.js"></script>
+        <link rel="stylesheet" href="css/uikit.min.css" type="text/css">
         <title>
             Fryday Nite
         </title>
     </head>
     <body>
-        
+        <nav class="uk-navbar-container uk-margin" uk-navbar>
+            <div class="uk-navbar-center">
+                <a class="uk-navbar-item uk-logo" href="">Fryday Nite</a>
+            </div>
+            <div class="uk-navbar-right">
+                <a href="" uk-icon="icon: question; ratio:2">Help</a>
+            </div>
+        </nav>
     <?php
        
         foreach ($db->query('SELECT first_name, last_name FROM users') as $user)
