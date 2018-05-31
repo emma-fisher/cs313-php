2a5da9b471585c8c0961eab6ca66a4e36ebdff4b
Emma Fisher <emmarfisher99@gmail.com>
2018-05-31 16:06:51 -0600

    project

--- web/Project/website.php
+++ web/Project/website.php
@@ -8,11 +8,11 @@
         die("DB Connection was not set");
     }
 
-    $query = "SELECT first_name FROM users";
+    $query = "SELECT title FROM posts";
     $statement = $db->prepare($query);
     //Bind any variables I need to
     $statement->execute();
-    $users = $statement->fetchAll(PDO::FETCH_ASSOC);
+    $posts = $statement->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <!DOCTYPE html>
 <html>
@@ -25,58 +25,46 @@
         </title>
     </head>
     <body>
+
+   
         
     <nav class="uk-navbar-container uk-margin" uk-navbar>
         <div class="uk-navbar-center">
             <a class="uk-navbar-item uk-logo" href="">Fryday Nite</a>
         </div>
         <div class="uk-navbar-right">
-            <a class="uk-navbar-item" href="">Hello 
-            <?php 
-            var_dump($users);
-                
-                // foreach ($db->query('SELECT first_name FROM users') as $user)
-                // {
-                //     $first_name = $user["first_name"];
-                    
-                //     if ($first_name == 'Emma')
-                //     {
-                //         echo $first_name;
-                //     }
-                    
-                // }
-            ?> </a>
+            <a class="uk-navbar-item" href="">Hello </a>
         </div>
     </nav>
+
+     <?php 
+            // var_dump($users);
+                
+        // foreach ($users as $user)
+        // {
+        //     echo "";       
+        // }
+    ?>
+
+    <?php 
+        foreach ($posts as $post)
+        {
+            $title = $post["title"];
+                        
+            echo "<div>
+                    <div class="uk-card uk-card-default uk-card-body">
+                        <h3 class="uk-card-title"></h3>
+                        <p>" . $title . "</p>
+                    </div>
+                </div>"
+        }
+    ?>
 
     <div class="uk-child-width-1-3@m uk-grid-small uk-grid-match" uk-grid>
         <div>
             <div class="uk-card uk-card-default uk-card-body">
-                <h3 class="uk-card-title">
-                <?php 
-                    foreach ($db->query('SELECT title FROM posts') as $post)
-                    {
-                        $title = $post["title"];
-                        
-                        if ($title == 'Walmart')
-                        {
-                            echo $title;
-                        }
-                    }
-                ?>
-                </h3>
-                <p><?php 
-                    foreach ($db->query('SELECT title, description_text FROM posts') as $post)
-                    {
-                        $description = $post["description_text"];
-                        $title = $post["title"];
-                        
-                        if ($title == 'Walmart')
-                        {
-                            echo $description;
-                        }
-                    }
-                ?></p>
+                <h3 class="uk-card-title"></h3>
+                <p></p>
             </div>
         </div>
         <div>
