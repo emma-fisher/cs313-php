9a6c3de828593f71ae00567b12dbdcd3140b5fd2
Emma Fisher <emmarfisher99@gmail.com>
2018-05-31 15:53:02 -0600

    project

--- web/Project/website.php
+++ web/Project/website.php
@@ -33,9 +33,7 @@
         <div class="uk-navbar-right">
             <a class="uk-navbar-item" href="">Hello 
             <?php 
-                $user = $db->query('SELECT first_name FROM users');
-                $first_name = $user["first_name"];
-                echo $first_name;
+            var_dump($users);
                 
                 // foreach ($db->query('SELECT first_name FROM users') as $user)
                 // {
