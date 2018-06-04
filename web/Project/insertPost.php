4c29aaf9129e656556fffc719f0557679ade6bfc
Emma Fisher <emmarfisher99@gmail.com>
2018-06-04 17:50:57 -0600

    working on project

--- web/Project/insertPost.php
+++ web/Project/insertPost.php
@@ -5,19 +5,36 @@
     $category = htmlspecialchars($_POST["category"]);	
     $tips = htmlspecialchars($_POST["tips"]);
 
-    echo "title: $title\n";
-    echo "description: $description\n";
-    echo "price: $price\n";
-    echo "category: $category\n";
-    echo "tips: $tips\n";
+    // echo "title: $title\n";
+    // echo "description: $description\n";
+    // echo "price: $price\n";
+    // echo "category: $category\n";
+    // echo "tips: $tips\n";
+
+    $categoryId;
+    if ($category == "outdoor") {
+        $categoryId = 1;
+    };
+    else if ($category == "service") {
+        $categoryId = 2;
+    };
+    else if ($category == "romantic") {
+        $categoryId = 3;
+    };
+    else if ($category == "group date") {
+        $categoryId = 4;
+    };
+
     require("dbConnect.php");
     $db = get_db();
-    // $query = "INSERT INTO note (course_id, content, date) VALUES (:courseId, :content, :date)";
-    // $statement = $db->prepare($query);
-    // $statement->bindValue(":courseId", $courseId, PDO::PARAM_INT);
-    // $statement->bindValue(":content", $content, PDO::PARAM_STR);
-    // $statement->bindValue(":date", $date, PDO::PARAM_STR);
-    // $statement->execute();
-    // header("Location: courseDetails.php?course_id=$courseId");
-    // die();
+    $query = "INSERT INTO posts (category_id, title, description_text, price, tips) VALUES (:categoryId, :title, :description, :price, :tips)";
+    $statement = $db->prepare($query);
+    $statement->bindValue(":categoryId", $categoryId, PDO::PARAM_INT);
+    $statement->bindValue(":title", $title, PDO::PARAM_STR);
+    $statement->bindValue(":description", $description, PDO::PARAM_STR);
+    $statement->bindValue(":price", $price, PDO::PARAM_INT);
+    $statement->bindValue(":tips", $tips, PDO::PARAM_STR);
+    $statement->execute();
+    //header("Location: courseDetails.php?course_id=$courseId");
+    die();
 ?>
