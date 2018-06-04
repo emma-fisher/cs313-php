472c118864dd2479f2df06e0e6bdc3bce2a5049f
Emma Fisher <emmarfisher99@gmail.com>
2018-06-04 12:16:23 -0600

    project stuff

--- web/Project/createPost.php
+++ web/Project/createPost.php
@@ -9,13 +9,42 @@ $courseId = htmlspecialchars($_GET["course_id"]);
 </head>
 <body>
 
-<form action="insertPost.php" method="POST">
-<input type="hidden" name="course_id" value="<?php echo $courseId; ?>">
+<!-- <form action="insertPost.php" method="POST">
+<input type="hidden" name="course_id" value="<?php //echo $courseId; ?>">
 <input type="date" name="date"><br>
 <textarea name="content" placeholder="Content"></textarea>
 
 <br><br>
 <input type="submit" value="Add Note">
+</form> -->
+
+<form>
+    <fieldset class="uk-fieldset">
+
+        <legend class="uk-legend">Create Post</legend>
+
+        <div class="uk-margin">
+            <input class="uk-input" type="text" placeholder="Title">
+        </div>
+
+        <div class="uk-margin">
+            <textarea class="uk-textarea" rows="5" placeholder="Description"></textarea>
+        </div>
+
+        <div class="uk-margin">
+            <label><input type="number">Price</label>
+        </div>
+
+        <div class="uk-margin">
+            <textarea class="uk-textarea" rows="5" placeholder="Tips"></textarea>
+        </div>
+
+        <div class="uk-margin uk-grid-small uk-child-width-auto uk-grid">
+            <label><input class="uk-checkbox" type="checkbox" checked> A</label>
+            <label><input class="uk-checkbox" type="checkbox"> B</label>
+        </div>
+
+    </fieldset>
 </form>
 
 </body>
