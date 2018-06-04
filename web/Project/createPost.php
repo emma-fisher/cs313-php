8a348ffd7dbe491623d1ef90ba585da8b78a3191
Emma Fisher <emmarfisher99@gmail.com>
2018-06-04 12:25:01 -0600

    project stuff

--- web/Project/createPost.php
+++ web/Project/createPost.php
@@ -4,48 +4,53 @@ $courseId = htmlspecialchars($_GET["course_id"]);
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-	<title></title>
-</head>
-<body>
+    <head>
+        <script src="js/uikit.min.js"></script>
+        <script src="js/uikit-icons.js"></script>
+        <link rel="stylesheet" href="css/uikit.min.css" type="text/css">
+        <title></title>
+    </head>
+    <body>
 
-<!-- <form action="insertPost.php" method="POST">
-<input type="hidden" name="course_id" value="<?php //echo $courseId; ?>">
-<input type="date" name="date"><br>
-<textarea name="content" placeholder="Content"></textarea>
+        <!-- <form action="insertPost.php" method="POST">
+        <input type="hidden" name="course_id" value="<?php //echo $courseId; ?>">
+        <input type="date" name="date"><br>
+        <textarea name="content" placeholder="Content"></textarea>
 
-<br><br>
-<input type="submit" value="Add Note">
-</form> -->
+        <br><br>
+        <input type="submit" value="Add Note">
+        </form> -->
 
-<form>
-    <fieldset class="uk-fieldset">
+        <form action="insertPost.php" method="POST">
+            <fieldset class="uk-fieldset">
 
-        <legend class="uk-legend">Create Post</legend>
+                <legend class="uk-legend">Create Post</legend>
 
-        <div class="uk-margin">
-            <input class="uk-input" type="text" placeholder="Title">
-        </div>
+                <div class="uk-margin">
+                    <input class="uk-input" type="text" placeholder="Title">
+                </div>
 
-        <div class="uk-margin">
-            <textarea class="uk-textarea" rows="5" placeholder="Description"></textarea>
-        </div>
+                <div class="uk-margin">
+                    <textarea class="uk-textarea" rows="5" placeholder="Description"></textarea>
+                </div>
 
-        <div class="uk-margin">
-            <label><input type="number">Price</label>
-        </div>
+                <div class="uk-margin">
+                    <label><input type="number">Price</label>
+                </div>
 
-        <div class="uk-margin">
-            <textarea class="uk-textarea" rows="5" placeholder="Tips"></textarea>
-        </div>
+                <div class="uk-margin">
+                    <textarea class="uk-textarea" rows="5" placeholder="Tips"></textarea>
+                </div>
 
-        <div class="uk-margin uk-grid-small uk-child-width-auto uk-grid">
-            <label><input class="uk-checkbox" type="checkbox" checked> A</label>
-            <label><input class="uk-checkbox" type="checkbox"> B</label>
-        </div>
+                <div class="uk-margin uk-grid-small uk-child-width-auto uk-grid">
+                    <label><input class="uk-checkbox" type="checkbox" checked> A</label>
+                    <label><input class="uk-checkbox" type="checkbox"> B</label>
+                </div>
 
-    </fieldset>
-</form>
+                <input type="submit" value="Create">
 
-</body>
+            </fieldset>
+        </form>
+    </body>
+
 </html>
