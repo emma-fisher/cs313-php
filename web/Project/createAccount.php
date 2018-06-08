fc07aee2590216db3c672bff841aad343581720c
Emma Fisher <emmarfisher99@gmail.com>
2018-06-08 17:03:19 -0600

    finishing login

--- web/Project/createAccount.php
+++ web/Project/createAccount.php
@@ -4,8 +4,7 @@
 // get the data from the POST
 $email = htmlspecialchars($_POST['email']);
 $password = htmlspecialchars($_POST['password']);
-echo $email;
-echo $password;
+
 // Get the hashed password.
 $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 // Connect to the database
@@ -18,8 +17,5 @@ $statement->bindValue(':password', $hashedPassword, PDO::PARAM_STR);
 $statement->execute();
 // finally, redirect them to the sign in page
 header("Location: signIn.php");
-die(); // we always include a die after redirects. In this case, there would be no
-       // harm if the user got the rest of the page, because there is nothing else
-       // but in general, there could be things after here that we don't want them
-       // to see.
+die();
 ?>
--- web/Project/signIn.php
+++ web/Project/signIn.php
@@ -41,6 +41,7 @@ if (isset($_POST['txtUser']) && isset($_POST['txtPassword']))
 			// password was correct, put the user on the session, and redirect to home
 			$_SESSION['username'] = $username;
 			header("Location: website.php");
+			exit();
 			die(); // we always include a die after redirects.
 		}
 		else
