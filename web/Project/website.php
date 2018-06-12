fa3dc60a9999e77139a9ed5d6583d81c6d6d6571
Emma Fisher <emmarfisher99@gmail.com>
2018-06-12 00:08:31 -0600

    project stuff

--- web/Project/website.php
+++ web/Project/website.php
@@ -32,6 +32,7 @@
         <script src="js/uikit.min.js"></script>
         <script src="js/uikit-icons.js"></script>
         <link rel="stylesheet" href="css/uikit.min.css" type="text/css">
+        <link rel="stylesheet" href="website.css" type="text/css">
         <title>
             Friday Nite
         </title>
