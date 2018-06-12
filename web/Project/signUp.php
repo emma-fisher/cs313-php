20f76e566817e34e587aa8691a672f131a5dc37b
Emma Fisher <emmarfisher99@gmail.com>
2018-06-11 23:46:22 -0600

    project stuff

--- web/Project/signUp.php
+++ web/Project/signUp.php
@@ -3,35 +3,38 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<script src="js/uikit.min.js"></script>
+    <script src="js/uikit-icons.js"></script>
+    <link rel="stylesheet" href="css/uikit.min.css" type="text/css">
+	<link rel="stylesheet" href="website.css" type="text/css">
 	<title>Sign Up</title>
 </head>
 
 <body>
 <div>
 
-<h1>Sign up for new account</h1>
+<div class="uk-card uk-card-default uk-card-body uk-width-1-4@m uk-position-center">
+    <h3 class="uk-card-title">Sign up for a new account:</h3>
 
-<form id="mainForm" action="createAccount.php" method="POST">
+    <form id="mainForm" action="createAccount.php" method="POST">
 
 	<input type="text" name="first_name" placeholder="First Name">
-	<label for="first_name">First Name</label>
 	<br /><br />
 
 	<input type="text" name="last_name" placeholder="Last Name">
-	<label for="last_name">Last Name</label>
 	<br /><br />
 
-	<input type="email" name="email" placeholder="Username">
-	<label for="email">Email</label>
+	<input class= "uk-input" type="email" name="email" placeholder="Email">
 	<br /><br />
 
-	<input type="password" name="password" placeholder="Password"></input>
-	<label for="password">Password</label>
+	<input class="uk-input" type="password" name="password" placeholder="Password">
 	<br /><br />
 
-	<input type="submit" value="Create Account" />
+	<input class="uk-input uk-width-1-2 uk-position-center" id="sign-in-button" type="submit" value="Create Account" />
 
-</form>
+	</form>
+
+</div>
 
 
 </div>
