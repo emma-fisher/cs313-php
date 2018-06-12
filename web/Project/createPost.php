df17901808224bf9e94f801bb1f7f6e657af51e3
Emma Fisher <emmarfisher99@gmail.com>
2018-06-12 00:22:02 -0600

    project stuff

--- web/Project/createPost.php
+++ web/Project/createPost.php
@@ -18,19 +18,19 @@ $courseId = htmlspecialchars($_GET["course_id"]);
                 <legend class="uk-legend">Create Post</legend>
 
                 <div class="uk-margin">
-                    <input class="uk-input" type="text" name="title" placeholder="Title">
+                    <input class="uk-input uk-width-1-2" type="text" name="title" placeholder="Title">
                 </div>
 
                 <div class="uk-margin">
-                    <textarea class="uk-textarea" rows="5" name="description" placeholder="Description"></textarea>
+                    <textarea class="uk-textarea uk-width-1-2" rows="5" name="description" placeholder="Description"></textarea>
                 </div>
 
                 <div class="uk-margin">
-                    Price<input type="number" name="price">
+                    Price<input type="number uk-width-1-2" name="price">
                 </div>
 
                 <div class="uk-margin">
-                    <textarea class="uk-textarea" rows="5" name="tips" placeholder="Tips"></textarea>
+                    <textarea class="uk-textarea uk-width-1-2" rows="5" name="tips" placeholder="Tips"></textarea>
                 </div>
 
                 <div class="uk-margin uk-grid-small uk-child-width-auto uk-grid">
@@ -40,7 +40,7 @@ $courseId = htmlspecialchars($_GET["course_id"]);
                     <label><input class="uk-radio" type="radio" name="category" value="group date"> Group Date</label>
                 </div>
 
-                <input class="uk-button-default" type="submit" value="Create">
+                <input class="uk-button-default uk-width-1-6" type="submit" value="Create">
 
             </fieldset>
         </form>
