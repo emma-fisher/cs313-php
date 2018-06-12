002d662f29971b91402e8fb2bfd6a09eca35b12d
Emma Fisher <emmarfisher99@gmail.com>
2018-06-12 00:41:36 -0600

    project stuff

--- web/Project/createPost.php
+++ web/Project/createPost.php
@@ -12,38 +12,41 @@ $courseId = htmlspecialchars($_GET["course_id"]);
     </head>
     <body>
     
-        <form action="insertPost.php" method="POST">
-            <fieldset class="uk-fieldset">
+    <div class="uk-card uk-card-default uk-card-body uk-width-1-2@m">
+        <h3 class="uk-card-title">Create Post</h3>
 
-                <legend class="uk-legend">Create Post</legend>
+            <form action="insertPost.php" method="POST">
+                <fieldset class="uk-fieldset">
 
-                <div class="uk-margin">
-                    <input class="uk-input uk-width-1-2" type="text" name="title" placeholder="Title">
-                </div>
 
-                <div class="uk-margin">
-                    <textarea class="uk-textarea uk-width-1-2" rows="5" name="description" placeholder="Description"></textarea>
-                </div>
+                    <div class="uk-margin">
+                        <input class="uk-input uk-width-1-2" type="text" name="title" placeholder="Title">
+                    </div>
 
-                <div class="uk-margin">
-                    Price<input type="number uk-width-1-2" name="price">
-                </div>
+                    <div class="uk-margin">
+                        <textarea class="uk-textarea uk-width-1-2" rows="5" name="description" placeholder="Description"></textarea>
+                    </div>
 
-                <div class="uk-margin">
-                    <textarea class="uk-textarea uk-width-1-2" rows="5" name="tips" placeholder="Tips"></textarea>
-                </div>
+                    <div class="uk-margin">
+                        Price<input type="number uk-width-1-2" name="price">
+                    </div>
 
-                <div class="uk-margin uk-grid-small uk-child-width-auto uk-grid">
-                    <label><input class="uk-radio" type="radio" name="category" value="outdoor"> Outdoor</label>
-                    <label><input class="uk-radio" type="radio" name="category" value="service"> Service</label>
-                    <label><input class="uk-radio" type="radio" name="category" value="romantic"> Romantic</label>
-                    <label><input class="uk-radio" type="radio" name="category" value="group date"> Group Date</label>
-                </div>
+                    <div class="uk-margin">
+                        <textarea class="uk-textarea uk-width-1-2" rows="5" name="tips" placeholder="Tips"></textarea>
+                    </div>
 
-                <input class="uk-button-default uk-width-1-4" type="submit" value="Create">
+                    <div class="uk-margin uk-grid-small uk-child-width-auto uk-grid">
+                        <label><input class="uk-radio" type="radio" name="category" value="outdoor"> Outdoor</label>
+                        <label><input class="uk-radio" type="radio" name="category" value="service"> Service</label>
+                        <label><input class="uk-radio" type="radio" name="category" value="romantic"> Romantic</label>
+                        <label><input class="uk-radio" type="radio" name="category" value="group date"> Group Date</label>
+                    </div>
 
-            </fieldset>
-        </form>
+                    <input class="uk-button-default uk-width-1-4" type="submit" value="Create">
+
+                </fieldset>
+            </form>
+        </div>
     </body>
 
 </html>
